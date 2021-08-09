14242f00f0960c93c5d5907620f7d9adade4e7ed
mfalvarez <mfalvarezc@unal.edu.co>
2021-08-09 15:35:17 -0500

    Static method for Cheking 4 in line horizontal.

--- js/Checker.js
+++ js/Checker.js
@@ -1,4 +1,22 @@
 class Checker{
-	
+
+	static isHortwin(board){
+		var l_return = false;
+		for(var r = board.length - 1; r >= 0; r--){
+			for(var c = 0; c <= board[0].length - 4; c++){
+
+				if ((board[r][c]   !== 0 &&
+			     	 board[r][c+1] !== 0 &&
+			     	 board[r][c+2] !== 0 &&
+			     	 board[r][c+3] !== 0) 
+					&& 
+					(board[r][c]   === board[r][c+1] &&
+			   	 	 board[r][c+1] === board[r][c+2] &&
+			   	 	 board[r][c+2] === board[r][c+3])) {l_return = true; break;}
+
+			}
+		}
+		return l_return;
+	}
 	
 }
